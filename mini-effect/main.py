752b14042d38d84ac9dbe36e16097f9eb62d3549
isolomonleecode <git.control742@passmail.net>
2026-03-30 22:39:51 -0500

    tested logging

--- mini-effect/main.py
+++ mini-effect/main.py
@@ -2,22 +2,26 @@ import pygame
 from constants import *
 from logger import log_state
 
+print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pass
+
+        dt = clock.tick(60) / 1000     
         screen.fill("red")
         pygame.display.flip()
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-
+        
 
 if __name__ == "__main__":
     main()
